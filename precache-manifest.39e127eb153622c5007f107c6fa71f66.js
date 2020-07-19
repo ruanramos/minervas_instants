388f5e42692b74b4f6d470569db5b8a57be17a0d
Ruan Ramos <ruan.ramos28@gmail.com>
2020-07-19 13:08:04 -0300

    Updates

---
+++ precache-manifest.39e127eb153622c5007f107c6fa71f66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384297b39e654275cf9c4e179aae3646",
+    "url": "/minervas_instants/index.html"
+  },
+  {
+    "revision": "2a220ee515d39a05c522",
+    "url": "/minervas_instants/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ae84214a1a95e332d8d4",
+    "url": "/minervas_instants/static/js/2.3e255f42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minervas_instants/static/js/2.3e255f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a220ee515d39a05c522",
+    "url": "/minervas_instants/static/js/main.8454e654.chunk.js"
+  },
+  {
+    "revision": "38396da757fe79fad655",
+    "url": "/minervas_instants/static/js/runtime-main.b2fd1321.js"
+  },
+  {
+    "revision": "16f1923a1c92036496428caa01ef31d3",
+    "url": "/minervas_instants/static/media/s4zlNEuh_400x400.16f1923a.jpg"
+  },
+  {
+    "revision": "33d8229cf20bf01acbf40604f52420ae",
+    "url": "/minervas_instants/static/media/soco.33d8229c.ogg"
+  }
+]);
